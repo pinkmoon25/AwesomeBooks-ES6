42f6831ae0012595c194cafe8c566fb1818cbe73
Tushar <singhtushar348@gmail.com>
2022-07-04 13:07:06 +0100

    Add and import modules

---
+++ index.js
@@ -0,0 +1,27 @@
+import { Book } from './modules/booklist.js'
+import { navigation } from './modules/navlink.js'
+import { success } from './modules/success.js'
+
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const addBtn = document.getElementById('add');
+
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (title.value === '' || author.value === '') return;
+
+  const bookId = Math.floor(Math.random() * 1000);
+  const newBook = new Book(title.value, author.value, bookId);
+  Book.addBook(newBook);
+  Book.storeBook(newBook);
+  success();
+  title.value = '';
+  author.value = '';
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  Book.getBook();
+  Book.displayBooks();
+});
+
+navigation();
